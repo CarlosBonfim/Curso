57bfdbfaaca79648c60445ef29e9517e7d96b360
CarlosBonfim <brunobonfim10@hotmail.com>
2020-07-30 11:37:42 -0400

    Foram feitas ataulizaçoes no projeto
    Atualizações

--- fundamentos/array.js
+++ fundamentos/array.js
@@ -17,3 +17,14 @@ console.log(valores)
 
 console.log(typeof valores)// função  typeoff serve para dizer o tipo da variavel
 
+async function init() {
+    console.log(1);
+    await sleep(1000);
+    console.log(2);
+}
+
+function sleep(ms) {
+    return new Promise((resolve) => {
+        setTimeout(resolve, ms);
+    });
+}  
